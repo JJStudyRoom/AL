824978a3d36cbaac5fd287dd06a878c2bdaec644
jiwook <kjo2354@naver.com>
2022-01-30 16:01:50 +0900

    [ 22.1.30 ] 완전탐색기법2문제- jaen

--- src/20220130/je/Four4.js
+++ src/20220130/je/Four4.js
@@ -1,6 +1,35 @@
-function solution(param){
-    let answer = null
+const solution = (param) => (pArr)=> {
+    let answer = 0  //maxCnt
+    let cnt = pArr.length
+    const limit = param
+
+    if(pArr.length){
+        const arr = pArr.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]))
+
+        for(let i=0; i < cnt; i++){
+            let discountTarget = arr[i][0] / 2
+            let sum = discountTarget + arr[i][1]
+            let maxCnt = 1
+
+            for(let j= 0; j < cnt; j++){
+                if(i !== j){
+                    const newSum = sum + arr[j][0] + arr[j][1]
+
+                    if(limit >= newSum){
+                        sum = newSum
+                        maxCnt += 1;
+                        
+                        if(limit === newSum){
+                            answer = maxCnt > answer ? maxCnt : answer
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     return answer
 }
 
-console.log(solution())
+console.log(solution(28)([[6,6], [2,2], [4,3], [4,5], [10,3]]))
--- src/20220130/je/Four5.js
+++ src/20220130/je/Four5.js
@@ -1,6 +1,20 @@
-function solution(param){
-    let answer = null
+const solution = (order) => (arr)=> {
+    let answer = 0
+    let sums = {}
+    
+    for(let i=0; i < arr.length - 2; i++){
+        for(let j=i+1; j < arr.length - 1; j++){
+            for(let k=j+1; k < arr.length; k++){
+                const sum = arr[i] + arr[j] + arr[k]
+                sums[sum] = 1
+            }
+        }
+    }
+    sums = Object.keys(sums).sort((a, b) => b - a)
+
+    answer = sums[order-1]
+
     return answer
 }
 
-console.log(solution())
+console.log(solution(3)([13, 15, 34, 23, 45, 65, 33, 11, 26, 42]))
