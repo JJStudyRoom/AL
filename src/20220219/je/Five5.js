07fec3a3ec7dcf8e177dff5354505350abf5a6d7
beckyi <beckyi@naver.com>
2022-02-20 20:37:44 +0900

    [22.02.19] update five5

--- src/20220219/je/Five5.js
+++ src/20220219/je/Five5.js
@@ -1,5 +1,5 @@
 /** 최대 매출 */
-function solution (day, arr) {
+function solution(day, arr) {
     let max = 0
     let sum = 0
     let sumArr = []
@@ -19,4 +19,23 @@ function solution (day, arr) {
     return max
 }
 
+// (update) 미리 합산을 구한 상태에서 진행
+function solution2(day, arr) {
+    let max = 0
+    
+    if(arr){
+        let sum = arr.slice(0, day).reduce((a, b) => a + b)
+
+        for(let i= day; i < arr.length; i++){
+            const target = arr[i]
+            sum = sum + target - arr[i - day]
+            max = max > sum ? max : sum
+        }
+
+    }
+
+    return max
+}
+
 console.log(solution(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]))
+console.log(solution2(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]))
